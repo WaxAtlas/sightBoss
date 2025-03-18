e27ad451c2a4f73561c238d67256830fcc7ae6af
Watson Carawon <carawonw@gmail.com>
2025-03-18 13:35:01 -0400

    Refactor and add cell click functionality
    Some minor refactoring to hopefully make things a little more readable.
    Also added a click listener to each cell to toggle state - easy MVP.

--- main.js
+++ main.js
@@ -19,27 +19,27 @@ function addArrowKeys() {
     upButton.innerHTML = "↑";
     upButton.setAttribute("class", "arrowKey");
     upButton.addEventListener("click", upButtonHandler);
-    
+
     let downButton = document.createElement("button");
     downButton.innerText = "↓";
     downButton.setAttribute("class", "arrowKey");
     downButton.addEventListener("click", downButtonHandler);
-    
+
     let leftButton = document.createElement("button");
     leftButton.innerText = "←";
     leftButton.setAttribute("class", "arrowKey");
     leftButton.addEventListener("click", leftButtonHandler);
-    
+
     let rightButton = document.createElement("button");
     rightButton.innerText = "→";
     rightButton.setAttribute("class", "arrowKey");
     rightButton.addEventListener("click", rightButtonHandler);
-    
+
     let topRow = document.createElement("div");
     topRow.setAttribute("class", "row");
     topRow?.appendChild(upButton);
     arrowKeys?.appendChild(topRow);
-    
+
     let bottomRow = document.createElement("div");
     bottomRow.setAttribute("class", "row");
     bottomRow?.appendChild(leftButton);
@@ -50,7 +50,7 @@ function addArrowKeys() {
 
 function createGrid(N, islands) {
     let cellHeight, cellWidth;
-    if (board?.clientHeight && board.clientWidth){
+    if (board?.clientHeight && board.clientWidth) {
         cellHeight = Math.floor(board?.clientHeight / N) - 2;
         cellWidth = Math.floor(board?.clientWidth / N) - 2;
     }
@@ -58,8 +58,6 @@ function createGrid(N, islands) {
         cellHeight = 10;
         cellWidth = 10;
     }
-
-    console.log(islands);
 
     const squareSize = ((cellHeight < cellWidth) ? cellHeight : cellWidth).toString() + "px";
 
@@ -71,22 +69,22 @@ function createGrid(N, islands) {
 
         for (let j = 0; j < N; j++) {
             gridState[i][j] = "";
-        
-            let square = document.createElement("div");
-            square.style.height = squareSize;
-            square.style.width = squareSize;
-            square.setAttribute("class", "cell");
-            square.dataset.row = i.toString();
-            square.dataset.col = j.toString();
+
+            let cell = document.createElement("div");
+            cell.style.height = squareSize;
+            cell.style.width = squareSize;
+            cell.setAttribute("class", "cell");
+            cell.dataset.row = i.toString();
+            cell.dataset.col = j.toString();
 
             let isIsland = false;
             islands.forEach(island => {
-                if (island[0] === i && island[1] === j) {
+                if (island.row === i && island.col === j) {
                     isIsland = true;
                 }
             });
             if (isIsland) {
-                square.style.backgroundColor = "black";
+                cell.style.backgroundColor = "black";
                 // square.innerText = "island";
 
                 gridState[i][j] = "island";
@@ -94,9 +92,10 @@ function createGrid(N, islands) {
             else {
                 // square.innerText = "X";
                 gridState[i][j] = "X";
+                cell.addEventListener("click", cellButtonHandler);
             }
 
-            row.appendChild(square);
+            row.appendChild(cell);
         }
         grid?.appendChild(row);
     }
@@ -110,13 +109,26 @@ function tbtButtonHandler(e) {
     addArrowKeys();
 
     let islands;
-    switch(e.currentTarget.innerText) {
+    switch (e.currentTarget.innerText) {
         case "Alpha":
-            islands = [[1, 5], [2, 1], [3, 8], [4, 3], [5, 1], [6, 8], [7, 3], [7, 5]];
+            islands = [
+                { "row": 1, "col": 5 }, { "row": 2, "col": 1 }, { "row": 3, "col": 8 },
+                { "row": 4, "col": 3 }, { "row": 5, "col": 1 }, { "row": 6, "col": 8 },
+                { "row": 7, "col": 3 }, { "row": 7, "col": 5 },
+            ]
             break;
         case "Bravo":
+            islands = [
+                { "row": 1, "col": 8 }, { "row": 2, "col": 1 }, { "row": 3, "col": 6 },
+                { "row": 4, "col": 4 }, { "row": 6, "col": 6 }, { "row": 7, "col": 2 },
+            ]
             break;
         case "Charlie":
+            islands = [
+                { "row": 2, "col": 3 }, { "row": 2, "col": 4 }, { "row": 2, "col": 5 },
+                { "row": 6, "col": 1 }, { "row": 7, "col": 6 }, { "row": 8, "col": 6 },
+                { "row": 8, "col": 7 },
+            ]
             break;
         default:
             console.log("Error");
@@ -133,12 +145,39 @@ function rtButtonHandler(e) {
     addArrowKeys();
 
     let islands;
-    switch(e.innerText) {
+    switch (e.currentTarget.innerText) {
         case "Alpha":
+            islands = [
+                { "row": 1, "col": 1 }, { "row": 1, "col": 2 }, { "row": 1, "col": 6 },
+                { "row": 1, "col": 11 }, { "row": 2, "col": 1 }, { "row": 2, "col": 8 },
+                { "row": 2, "col": 11 }, { "row": 3, "col": 8 }, { "row": 5, "col": 4 },
+                { "row": 5, "col": 14 }, { "row": 6, "col": 2 }, { "row": 6, "col": 11 },
+                { "row": 7, "col": 1 }, { "row": 7, "col": 6 }, { "row": 7, "col": 7 },
+                { "row": 7, "col": 8 }, { "row": 7, "col": 11 }, { "row": 8, "col": 3 },
+                { "row": 8, "col": 12 }, { "row": 9, "col": 4 }, { "row": 10, "col": 1 },
+                { "row": 10, "col": 7 }, { "row": 10, "col": 10 }, { "row": 11, "col": 1 },
+                { "row": 11, "col": 3 }, { "row": 11, "col": 13 }, { "row": 12, "col": 3 },
+                { "row": 12, "col": 6 }, { "row": 13, "col": 3 }, { "row": 13, "col": 7 },
+                { "row": 13, "col": 13 },
+            ]
             break;
         case "Bravo":
+            islands = [
+                { "row": 2, "col": 1 }, { "row": 2, "col": 2 }, { "row": 3, "col": 6 },
+                { "row": 3, "col": 10 }, { "row": 3, "col": 11 }, { "row": 3, "col": 12 },
+                { "row": 4, "col": 6 }, { "row": 5, "col": 10 }, { "row": 6, "col": 10 },
+                { "row": 7, "col": 2 }, { "row": 7, "col": 3 }, { "row": 7, "col": 5 },
+                { "row": 8, "col": 5 }, { "row": 9, "col": 5 }, { "row": 10, "col": 10 },
+                { "row": 12, "col": 2 }, { "row": 13, "col": 1 }, { "row": 13, "col": 13 },
+            ]
             break;
         case "Charlie":
+            islands = [
+                { "row": 3, "col": 2 }, { "row": 3, "col": 6 }, { "row": 4, "col": 1 },
+                { "row": 4, "col": 6 }, { "row": 4, "col": 7 }, { "row": 4, "col": 8 },
+                { "row": 7, "col": 2 }, { "row": 7, "col": 5 }, { "row": 8, "col": 5 },
+                { "row": 10, "col": 1 }, { "row": 11, "col": 2 },
+            ]
             break;
         default:
             console.log("Error");
@@ -165,10 +204,10 @@ function updateGridState(addElements, removeElements) {
     // console.log("removeElements", removeElements);
 
     addElements.forEach(element => {
-        gridState[element[0]][element[1]] = "X";
+        gridState[element.row][element.col] = "X";
     });
     removeElements.forEach(element => {
-        gridState[element[0]][element[1]] = "";
+        gridState[element.row][element.col] = "";
     });
 
     updateGrid();
@@ -187,18 +226,18 @@ function moveN() {
 
         if (row >= 1) {
             if (gridState[row - 1][col] === "") {
-                addElements.push([row - 1, col]);
+                addElements.push({ "row": row - 1, "col": col });
             }
         }
         if (row <= gridSize - 2) {
             if (gridState[row + 1][col] === "" || gridState[row + 1][col] === "island") {
                 if (gridState[row][col] != "island") {
-                    removeElements.push([row, col]);
+                    removeElements.push({ "row": row, "col": col });
                 }
             }
         }
         if (row === gridSize - 1) {
-            removeElements.push([row, col]);
+            removeElements.push({ "row": row, "col": col });
         }
     });
 
@@ -218,18 +257,18 @@ function moveE() {
 
         if (col <= 8) {
             if (gridState[row][col + 1] === "") {
-                addElements.push([row, col + 1]);
+                addElements.push({ "row": row, "col": col + 1 });
             }
         }
         if (col >= 1) {
             if (gridState[row][col - 1] === "" || gridState[row][col - 1] === "island") {
                 if (gridState[row][col] != "island") {
-                    removeElements.push([row, col]);
+                    removeElements.push({ "row": row, "col": col });
                 }
             }
         }
         if (col === 0) {
-            removeElements.push([row, col]);
+            removeElements.push({ "row": row, "col": col });
         }
     });
 
@@ -249,18 +288,18 @@ function moveS() {
 
         if (row <= 8) {
             if (gridState[row + 1][col] === "") {
-                addElements.push([row + 1, col]);
+                addElements.push({ "row": row + 1, "col": col });
             }
         }
         if (row >= 1) {
             if (gridState[row - 1][col] === "" || gridState[row - 1][col] === "island") {
                 if (gridState[row][col] != "island") {
-                    removeElements.push([row, col]);
+                    removeElements.push({ "row": row, "col": col });
                 }
             }
         }
         if (row === 0) {
-            removeElements.push([row, col]);
+            removeElements.push({ "row": row, "col": col });
         }
     });
 
@@ -280,18 +319,18 @@ function moveW() {
 
         if (col >= 1) {
             if (gridState[row][col - 1] === "") {
-                addElements.push([row, col - 1]);
+                addElements.push({ "row": row, "col": col - 1 });
             }
         }
         if (col <= gridSize - 2) {
             if (gridState[row][col + 1] === "" || gridState[row][col + 1] === "island") {
                 if (gridState[row][col] != "island") {
-                    removeElements.push([row, col]);
+                    removeElements.push({ "row": row, "col": col });
                 }
             }
         }
         if (col === gridSize - 1) {
-            removeElements.push([row, col]);
+            removeElements.push({ "row": row, "col": col });
         }
     });
 
@@ -318,6 +357,25 @@ function rightButtonHandler() {
     moveE();
 }
 
+function cellButtonHandler(e) {
+    let addElements = [];
+    let removeElements = [];
+
+    console.log("inner text: ", e.currentTarget.innerText)
+
+    if (e.currentTarget.innerText === "X") {
+        removeElements.push({ "row": e.currentTarget.dataset.row, "col": e.currentTarget.dataset.col });
+    }
+    else if (e.currentTarget.innerText === "") {
+        addElements.push({ "row": e.currentTarget.dataset.row, "col": e.currentTarget.dataset.col })
+    }
+
+    console.log("add elements: ", addElements);
+    console.log("remove elements: ", removeElements);
+
+    updateGridState(addElements, removeElements);
+}
+
 /** @type {HTMLDivElement | null} */
 const board = document.querySelector("#board");
 
@@ -329,8 +387,8 @@ const arrowKeys = document.querySelector("#arrowKeys");
 
 /** @type {NodeListOf<HTMLButtonElement>} */
 const tbtButtons = document.querySelectorAll(".tbtButton");
-tbtButtons.forEach(button => { button.addEventListener("click", tbtButtonHandler)})
+tbtButtons.forEach(button => { button.addEventListener("click", tbtButtonHandler) })
 
 /** @type {NodeListOf<HTMLButtonElement>} */
 const rtButtons = document.querySelectorAll(".rtButton");
-rtButtons.forEach(button => { button.addEventListener("click", rtButtonHandler)})
+rtButtons.forEach(button => { button.addEventListener("click", rtButtonHandler) })
